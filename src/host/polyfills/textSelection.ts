5154d5054037419d6a381579b4846d1fc008525e
Nikola R. Hristov <Nikola@Playform.Cloud>
2024-03-21 00:40:26 +0200

    squash!

--- src/host/polyfills/textSelection.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-export default function applySetupTextSelectionPatch(content: string) {
-    return content.replace(
-        /_setupTextSelectionHack\(stylePaneWrapperElement\)\s*{/g,
-        "_setupTextSelectionHack() { return;",
-    );
-}
